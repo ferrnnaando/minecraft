5b05162dc41482996776ca7f7fb6480ee34ac77a
ferrnnaando <77246868+ferrnnaando@users.noreply.github.com>
2023-09-04 20:18:39 -0700

    pendant to modify the menu to make it dinamic to work as a enum

--- vs_current/minecraft/header.h
+++ vs_current/minecraft/header.h
@@ -18,6 +18,14 @@
 
 #include "Utils/ResourceRandomizer.h"
 
-enum class MenuWidget { Title, Subtitle, Singleplayer, Multiplayer, Settings, Quit };
+enum class MenuWidget {  Title        = 1, 
+						 Subtitle     = 2, 
+						 Singleplayer = 3, 
+						 Multiplayer  = 4, 
+						 Settings     = 5, 
+						 Quit         = 6 
+					  };
+//std::vector<int> MenuWidgetExtend = { 0 };
+//std::vector<int> MenuWidgetExtend[];
 enum class gameState { Menu, Loading, SP_Gameplay, MP_Gameplay, Settings };
 enum class LoadState { ls_true, ls_false };
